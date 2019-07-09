12ed0259fe018f182d57c28c628552883d46a3c8
sixhops <sixhops@gmail.com>
2019-07-08 21:12:00 -0700

    Adding method-override

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const methodOverride = require('method-override');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('static'));
+app.use(methodOverride('_method'));
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
